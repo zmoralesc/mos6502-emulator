f662e1b8a588f13807f0f6d41a50e7a67c104e81
Zuriel Morales <zemc77@hotmail.com>
2023-08-19 16:41:24 -0600

    fixed status flag bit order

--- src/mos6502/mod.rs
+++ src/mos6502/mod.rs
@@ -22,13 +22,13 @@ pub trait Bus {
     fn size(&self) -> usize;
 }
 
-pub const FLAG_NEGATIVE: u8 = 1 << 0;
-pub const FLAG_OVERFLOW: u8 = 1 << 1;
-pub const FLAG_BREAK: u8 = 1 << 3;
-pub const FLAG_DECIMAL: u8 = 1 << 4;
-pub const FLAG_NO_INTERRUPTS: u8 = 1 << 5;
-pub const FLAG_ZERO: u8 = 1 << 6;
-pub const FLAG_CARRY: u8 = 1 << 7;
+pub const FLAG_NEGATIVE: u8 = 1 << 7;
+pub const FLAG_OVERFLOW: u8 = 1 << 6;
+pub const FLAG_BREAK: u8 = 1 << 4;
+pub const FLAG_DECIMAL: u8 = 1 << 3;
+pub const FLAG_NO_INTERRUPTS: u8 = 1 << 2;
+pub const FLAG_ZERO: u8 = 1 << 2;
+pub const FLAG_CARRY: u8 = 1 << 0;
 
 const STACK_BASE: u16 = 0x0100;
 
