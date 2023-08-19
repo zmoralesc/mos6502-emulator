9df546370dc710d0f64ae2c725e43d7cdd8d7bd2
Zuriel Morales <zemc77@hotmail.com>
2023-08-19 17:11:43 -0600

    fixed stack opcodes affecting the interrupt flag

--- src/mos6502/stack_ops.rs
+++ src/mos6502/stack_ops.rs
@@ -11,10 +11,7 @@ impl<T: Bus> MOS6502<T> {
 
     pub(super) fn php(&mut self, _: AddressingMode) -> Result<(), EmulationError> {
         let target_address: u16 = STACK_BASE + self.stack_pointer as u16;
-        self.write_to_bus(
-            target_address,
-            self.status_register | FLAG_BREAK | FLAG_NO_INTERRUPTS,
-        )?;
+        self.write_to_bus(target_address, self.status_register | FLAG_BREAK | (1 << 5))?;
         self.stack_pointer = self.stack_pointer.wrapping_sub(1);
         self.increment_cycles(3);
         Ok(())
@@ -33,8 +30,7 @@ impl<T: Bus> MOS6502<T> {
     pub(super) fn plp(&mut self, _: AddressingMode) -> Result<(), EmulationError> {
         self.stack_pointer = self.stack_pointer.wrapping_add(1);
         let target_address: u16 = STACK_BASE + self.stack_pointer as u16;
-        self.status_register =
-            self.read_from_bus(target_address)? & !FLAG_BREAK & !FLAG_NO_INTERRUPTS;
+        self.status_register = self.read_from_bus(target_address)? & !FLAG_BREAK & !(1 << 5);
         self.increment_cycles(4);
         Ok(())
     }
