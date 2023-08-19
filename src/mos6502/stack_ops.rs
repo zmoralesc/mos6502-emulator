d980a0f6216ed186f71f9c2637b988bac833f580
Zuriel Morales <zemc77@hotmail.com>
2023-08-19 00:54:02 -0600

    fixed stack-pull opcodes

--- src/mos6502/stack_ops.rs
+++ src/mos6502/stack_ops.rs
@@ -21,9 +21,9 @@ impl<T: Bus> MOS6502<T> {
     }
 
     pub(super) fn pla(&mut self, _: AddressingMode) -> Result<(), EmulationError> {
-        let target_address: u16 = STACK_BASE + self.stack_pointer as u16;
-        self.accumulator = self.read_from_bus(target_address)?;
         self.stack_pointer = self.stack_pointer.wrapping_add(1);
+        let target_address: u16 = STACK_BASE + ((self.stack_pointer as u16) & 0xFF);
+        self.accumulator = self.read_from_bus(target_address)?;
         self.increment_cycles(4);
         self.flag_toggle(FLAG_ZERO, self.accumulator == 0);
         self.flag_toggle(FLAG_NEGATIVE, self.accumulator & NEGATIVE_BIT_MASK != 0);
@@ -31,10 +31,10 @@ impl<T: Bus> MOS6502<T> {
     }
 
     pub(super) fn plp(&mut self, _: AddressingMode) -> Result<(), EmulationError> {
+        self.stack_pointer = self.stack_pointer.wrapping_add(1);
         let target_address: u16 = STACK_BASE + self.stack_pointer as u16;
         self.status_register =
             self.read_from_bus(target_address)? & !FLAG_BREAK & !FLAG_NO_INTERRUPTS;
-        self.stack_pointer = self.stack_pointer.wrapping_add(1);
         self.increment_cycles(4);
         Ok(())
     }
