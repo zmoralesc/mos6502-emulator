3fe53e2db74a044e4ebe2936c089b6c2052c1c92
Zuriel Morales <zemc77@hotmail.com>
2023-08-28 19:51:25 -0600

    replaced some more macros with inlined functions

--- src/mos6502/comparison_ops.rs
+++ src/mos6502/comparison_ops.rs
@@ -1,43 +1,47 @@
 use super::*;
 use std::cmp::Ordering;
 
-macro_rules! compare_register {
-    ($cpu:expr, $register:expr, $address_mode:ident, $bus:expr) => {
-        let operand: u8 = match $cpu.resolve_operand($bus, $address_mode)? {
+impl<T: Bus> MOS6502<T> {
+    #[inline(always)]
+    fn compare_register(
+        &mut self,
+        register: u8,
+        bus: &mut T,
+        address_mode: AddressingMode,
+    ) -> Result<(), EmulationError> {
+        let operand: u8 = match self.resolve_operand(bus, address_mode)? {
             OpcodeOperand::Byte(b) => b,
-            OpcodeOperand::Address(w) => $bus.read(w)?,
+            OpcodeOperand::Address(w) => bus.read(w)?,
             _ => return Err(EmulationError::InvalidAddressingMode),
         };
 
-        $cpu.increment_cycles(1);
-        let result = $register.wrapping_sub(operand);
-        $cpu.flag_toggle(FLAG_NEGATIVE, result & NEGATIVE_BIT_MASK != 0);
+        self.increment_cycles(1);
+        let result = register.wrapping_sub(operand);
+        self.flag_toggle(FLAG_NEGATIVE, result & NEGATIVE_BIT_MASK != 0);
 
-        match $register.cmp(&operand) {
+        match register.cmp(&operand) {
             Ordering::Less => {
-                $cpu.flag_toggle(FLAG_ZERO, false);
-                $cpu.flag_toggle(FLAG_CARRY, false);
+                self.flag_toggle(FLAG_ZERO, false);
+                self.flag_toggle(FLAG_CARRY, false);
             }
             Ordering::Equal => {
-                $cpu.flag_toggle(FLAG_ZERO, true);
-                $cpu.flag_toggle(FLAG_CARRY, true);
+                self.flag_toggle(FLAG_ZERO, true);
+                self.flag_toggle(FLAG_CARRY, true);
             }
             Ordering::Greater => {
-                $cpu.flag_toggle(FLAG_ZERO, false);
-                $cpu.flag_toggle(FLAG_CARRY, true);
+                self.flag_toggle(FLAG_ZERO, false);
+                self.flag_toggle(FLAG_CARRY, true);
             }
         }
-        return Ok(());
-    };
-}
+        Ok(())
+    }
 
-impl<T: Bus> MOS6502<T> {
     pub(super) fn cmp(
         &mut self,
         bus: &mut T,
         address_mode: AddressingMode,
     ) -> Result<(), EmulationError> {
-        compare_register!(self, self.accumulator, address_mode, bus);
+        self.compare_register(self.accumulator, bus, address_mode)
     }
 
     pub(super) fn cpx(
@@ -45,7 +49,7 @@ impl<T: Bus> MOS6502<T> {
         bus: &mut T,
         address_mode: AddressingMode,
     ) -> Result<(), EmulationError> {
-        compare_register!(self, self.x_register, address_mode, bus);
+        self.compare_register(self.x_register, bus, address_mode)
     }
 
     pub(super) fn cpy(
@@ -53,6 +57,6 @@ impl<T: Bus> MOS6502<T> {
         bus: &mut T,
         address_mode: AddressingMode,
     ) -> Result<(), EmulationError> {
-        compare_register!(self, self.y_register, address_mode, bus);
+        self.compare_register(self.y_register, bus, address_mode)
     }
 }
